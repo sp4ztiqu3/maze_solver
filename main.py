9240f72bb264b0fbd37d333709375232d7514d2e
Matt Hiddle <matt.hiddle@gmail.com>
2025-02-05 18:55:38 +0800

    setup repo, implement window spawning

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+    def __init__(self, width, height):
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(self.root, width=width, height=height)
+        self.canvas.pack()
+        self.is_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+
+    def wait_for_close(self):
+        self.is_running = True
+        while self.is_running:
+            self.redraw()
+
+    def close(self):
+        self.is_running = False
+
+def main():
+    window = Window(800, 600)
+    window.wait_for_close()
+
+main()
