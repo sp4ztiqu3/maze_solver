57934a1f1750aab7be76dce7df85421ae9e2c9d5
Matt Hiddle <matt.hiddle@gmail.com>
2025-02-05 19:03:39 +0800

    add Point and Line classes

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from tkinter import Tk, BOTH, Canvas
 
-class Window():
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point_1, point_2):
+        self.point_1 = point_1
+        self.point_2 = point_2
+
+    def draw(self, canvas, colour):
+        canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill=colour, width=2)
+
+
+class Window:
     def __init__(self, width, height):
         self.root = Tk()
         self.root.title("Maze Solver")
@@ -18,11 +32,16 @@ class Window():
         while self.is_running:
             self.redraw()
 
+    def draw_line(self, line, colour):
+        line.draw(self.canvas, colour)
+
     def close(self):
         self.is_running = False
 
 def main():
     window = Window(800, 600)
+    line1 = Line(Point(100, 300), Point(300, 100))
+    window.draw_line(line1, "red")
     window.wait_for_close()
 
 main()
